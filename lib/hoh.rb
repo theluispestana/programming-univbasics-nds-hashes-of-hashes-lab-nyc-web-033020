d9815c47815f1f907b7d1746bdb2bf96809caa8e
Luis Pestana <lpestana@protonmail.com>
2020-02-27 23:00:52 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,31 +4,31 @@
 #
 # The tests will guide your construction.
 #
-PORTION_1 = {
-  label: "Kingdom",
-  sub_category: {
-    label: "Phylum",
-    sub_category: {
-      label: "Class"
-    }
-  }
-}
-
-PORTION_2 = {
-  label: "Order"
-}
-
-PORTION_3 = {
-  label: "Family",
-  sub_category: {
-    label: "Genus",
-  }
-}
-
-PORTION_4 = {
-  label: "Species",
-  sub_category: nil
-}
+# PORTION_1 = {
+#   label: "Kingdom",
+#   sub_category: {
+#     label: "Phylum",
+#     sub_category: {
+#       label: "Class"
+#     }
+#   }
+# }
+#
+# PORTION_2 = {
+#   label: "Order"
+# }
+#
+# PORTION_3 = {
+#   label: "Family",
+#   sub_category: {
+#     label: "Genus",
+#   }
+# }
+#
+# PORTION_4 = {
+#   label: "Species",
+#   sub_category: nil
+# }
 
 def naming_system
   # Remember:
@@ -66,7 +66,7 @@ def naming_system
     label: "Species",
     sub_category: nil
   }
-  
+
   hoh = {
     portion_1: PORTION_1,
     portion_2: portion_2,
